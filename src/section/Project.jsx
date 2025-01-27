4213223d59c54ac1e8dbeccab91a964ca3760edf
AbbasLewa <abbasomerhassn@gmail.com>
2025-01-27 08:53:18 +0200

    Updated AboutMe, Hero, Project sections and added book.png

--- src/section/Project.jsx
+++ src/section/Project.jsx
@@ -1,6 +1,6 @@
 import Task from "../assets/Task.png";
 import pop from "../assets/pop.png";
-import clu from "../assets/cul.png";
+import book from "../assets/book.png";
 
 const Project = () => {
   return (
@@ -13,7 +13,7 @@ const Project = () => {
 
       <div className="flex flex-col md:flex-row justify-center items-center space-y-8 md:space-y-0 md:space-x-8">
         
-        <div className="w-full md:w-1/3 bg-gray-900 rounded-lg shadow-lg p-12 hover:scale-105 hover:rotate-3 transition-transform duration-300 wiggle">
+        <div className="w-full md:w-1/3 bg-gray-900 rounded-lg shadow-lg p-11 hover:scale-105 hover:rotate-3 transition-transform duration-300 wiggle">
           <h3 className="text-xl font-semibold text-yellow-600 mb-4 text-center">
           Task Tracker
           </h3>
@@ -24,7 +24,7 @@ const Project = () => {
           </p>
         </div>
 
-        <div className="w-full md:w-1/3 bg-gray-900 rounded-lg shadow-lg p-7 hover:scale-105 hover:translate-x-2 transition-transform duration-300 shake">
+        <div className="w-full md:w-1/3 bg-gray-900 rounded-lg shadow-lg p-9 hover:scale-105 hover:translate-x-2 transition-transform duration-300 shake">
           <h3 className="text-xl font-semibold text-yellow-600 mb-4 text-center">
           Portfolio
           </h3>
@@ -37,14 +37,14 @@ const Project = () => {
         </div>
         
 
-        <div className="w-full md:w-1/3 bg-gray-900 rounded-lg shadow-lg p-7 hover:scale-105 hover:skew-y-3 transition-transform duration-300 bounce-twice">
+        <div className="w-full md:w-1/3 bg-gray-900 rounded-lg shadow-lg p-16 hover:scale-105 hover:skew-y-3 transition-transform duration-300 bounce-twice">
           <h3 className="text-xl font-semibold text-yellow-600 mb-4 text-center">
-          Calculator
+          Bookmark
           </h3>
           <p className="text-sm text-center text-gray-400 mb-6">
-          <a href="https://calculator-btn-xliu.vercel.app/" target="_blank" rel="noopener noreferrer">
+          <a href="https://abbaslewa.github.io/bookmarks/" target="_blank" rel="noopener noreferrer">
 
-            <img src={clu} alt="clu" className="rounded-lg lg:ml-10"/>
+            <img src={book} alt="book" className="rounded-lg"/>
             </a>
           </p>
         </div>
